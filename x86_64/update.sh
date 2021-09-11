8907be42a0f6d793337456f2eb0cb59095f01298
fcanata <fcanata27@gmail.com>
2021-09-11 12:27:27 -0300

    config pacage

--- x86_64/update.sh
+++ x86_64/update.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-rm repo*
+rm archrepo*
 
+repo-add -n -R -q archrepo.db.tar.zst *.pkg.tar.zst
+rm archrepo.{db,files}
+cp -f archrepo.db.tar.zst archrepo.db
+cp -f archrepo.files.tar.zst archrepo.files
 
-echo "repo-add"
-repo-add -n -R repo.db.tar.gz *.pkg.tar.zst
+#echo "repo-add"
+#repo-add -n -R archrepo.db.tar.gz *.pkg.tar.zst
 #sleep 8
-cp -f repo.db.tar.gz repo.db
+#cp -f archrepo.db.tar.gz archrepo.db
 
 echo "####################################"
 echo "Repo Updated!!"
